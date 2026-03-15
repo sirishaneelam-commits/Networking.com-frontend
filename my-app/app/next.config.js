c71309d17def0e870c6c281e65243a03f7beee99
Sirishaneelam <https://github.com/sirishaneelam-commits/test-repo.git>
2026-03-16 00:05:34 +0530

    final commit

--- my-app/app/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["lh3.googleusercontent.com"], // allow Google profile images
-  },
-};
-
-module.exports = nextConfig;
